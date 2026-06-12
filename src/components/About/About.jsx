7c9b572ba5fd16803e233a6d4ee49beb73875ee4
Rodrigo Maturano Zanetti <rzanetti.dev@outlook.com>
2026-06-12 18:00:30 +0200

    feat: create about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import authorPhoto from "../../images/autor.jpg";
+
+function About() {
+  return (
+    <section className="about">
+      <img src={authorPhoto} alt="photo of the author" />
+      <h1>Sobre o autor</h1>
+      <p>
+        Esse bloco descreve o autor do projeto. Aqui você deve indicar seu nome,
+        o que você faz e quais tecnologias de desenvolvedor você conhece.
+      </p>
+      <p>
+        Você também pode falar sobre sua experiência com o Practicum, o que
+        aprendeu lá e como pode ajudar clientes em potencial.
+      </p>
+    </section>
+  );
+}
+
+export default About;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,13 @@
+import React from "react";
+import About from "../About/About";
+
 function Main() {
-  return <h1>Página Principal</h1>;
+  return (
+    <>
+      <h1>Página Principal</h1>
+      <About />
+    </>
+  );
 }
 
 export default Main;
