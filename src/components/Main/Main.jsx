0494e57e264e768ae93904bc808e97cbc49a3626
Rodrigo Maturano Zanetti <rzanetti.dev@outlook.com>
2026-06-12 18:12:41 +0200

    feat: add search form section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import About from "../About/About";
+import SearchForm from "../SearchForm/SearchForm";
 
 function Main() {
   return (
     <>
       <h1>Página Principal</h1>
+      <SearchForm />
       <About />
     </>
   );
---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function SearchForm() {
+  return (
+    <div className="searchform">
+      <h1 className="searchform__title">O que está acontecendo no mundo?</h1>
+      <h2 className="searchform__subtitle">Encontre as últimas notícias...</h2>
+
+      <form className="searchform__form">
+        <input
+          className="searchform__input"
+          type="text"
+          placeholder="Inserir tema"
+        />
+        <button className="searchform__button">Procurar</button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchForm;
