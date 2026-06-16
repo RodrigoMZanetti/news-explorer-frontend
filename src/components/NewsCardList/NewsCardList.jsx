0c10e55f061c3f989419a409c965b9a7282ffcbd
Rodrigo Maturano Zanetti <rzanetti.dev@outlook.com>
2026-06-16 11:13:46 +0200

    feat: add preloader in NewsCardList

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import NewsCard from "../NewsCard/NewsCard";
 import "./NewsCardList.css";
+import Preloader from "../Preloader/Preloader";
 
 function NewsCardList({ articles, isLoading, visibleCount }) {
   return (
     <div className="newscardlist">
       <h1 className="newscardlist__title">Procurar resultados</h1>
       <ul className="newscardlist__list">
-        {articles.map((article) => {
-          return (
-            <li key={article.url}>
-              <NewsCard
-                title={article.title}
-                date={article.publishedAt}
-                description={article.description}
-                source={article.source.name}
-                image={article.urlToImage}
-              />
-            </li>
-          );
-        })}
+        {isLoading === true ? (
+          <Preloader />
+        ) : (
+          articles.map((article) => {
+            return (
+              <li key={article.url}>
+                <NewsCard
+                  title={article.title}
+                  date={article.publishedAt}
+                  description={article.description}
+                  source={article.source.name}
+                  image={article.urlToImage}
+                />
+              </li>
+            );
+          })
+        )}
       </ul>
       <button className="newscardlist__button">Mostrar mais</button>
     </div>
